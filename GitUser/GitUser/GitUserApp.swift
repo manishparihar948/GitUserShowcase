385a91834d3f4d5ee65575678d8ecb168e1b6ca1
manishparihar948 <manish.parihar948@gmail.com>
2023-06-23 12:50:12 +0200

    Initial commit

---
+++ GitUser/GitUser/GitUserApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitUserApp.swift
+//  GitUser
+//
+//  Created by Manish Parihar on 23.06.23.
+//
+
+import SwiftUI
+
+@main
+struct GitUserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            GitUserListView()
+        }
+    }
+}
---
+++ GitUser/GitUser/GitUserListView.swift
@@ -0,0 +1,20 @@
+//
+//  GitUserListView.swift
+//  GitUser
+//
+//  Created by Manish Parihar on 23.06.23.
+//
+
+import SwiftUI
+
+struct GitUserListView: View {
+    var body: some View {
+        Text("Show Git User List")
+    }
+}
+
+struct GitUserListView_Previews: PreviewProvider {
+    static var previews: some View {
+        GitUserListView()
+    }
+}
